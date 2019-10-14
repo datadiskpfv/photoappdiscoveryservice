501fd55bde4d0598039b56f46ceec095bdd21b3f
Paul Valle <paul.valle@datadisk.co.uk>
2019-10-14 07:22:24 +0100

    Initial commit

---
+++ src/test/java/uk/co/datadisk/photoapp/discovery/photoappdiscoveryservice/PhotoappdiscoveryserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.co.datadisk.photoapp.discovery.photoappdiscoveryservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoappdiscoveryserviceApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
